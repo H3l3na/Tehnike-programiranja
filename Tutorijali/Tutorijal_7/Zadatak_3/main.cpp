5171eff5704d72d45c1eacc53b7574705fcbd00d
Alma Djedović <alma_djedovic04@hotmail.com>
2018-04-08 19:39:18 +0200

    Add files via upload

---
+++ Tutorijali/Tutorijal_7/Zadatak_3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <stdexcept>
+#include <new>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+template <typename tip>
+
+void SortirajListu(list<tip> l){
+
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
